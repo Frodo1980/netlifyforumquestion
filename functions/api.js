cd7eca145460886f7bbf52dd2173d549f57507fb
Michael Lüssem <34411791+Frodo1980@users.noreply.github.com>
2022-05-02 10:32:25 +0200

    .

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const XlsxPopulate = require("xlsx-populate");
-
+var cors = require("cors");
 const serverless = require("serverless-http");
 
 const app = express();
 const router = express.Router();
+app.use(cors());
+app.options("*", cors());
 
 router.get("/", (req, res) => {
   // Load an existing workbook
@@ -13,10 +15,10 @@ router.get("/", (req, res) => {
       const worksheet = workbook.sheet("Sheet1");
 
       const params = req.query;
+      console.table(params);
 
       // Modify the workbook.
       Object.keys(params).map((key, index) => {
-        console.log(Object.values(params)[index]);
         worksheet.cell(key).value(Object.values(params)[index]);
       });
 
@@ -24,7 +26,7 @@ router.get("/", (req, res) => {
     })
     .then((data) => {
       // Set the output file name.
-      res.attachment("output.xlsx");
+      //  res.attachment("output.xlsx");
 
       // Send the workbook.
       res.send(data);
